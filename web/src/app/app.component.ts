f41cec4d0ec79aef00cd7523922dca19a56de08f
FeralAlgae <cazocarma@gmail.com>
2025-10-23 16:23:05 -0300

    tarea: commit chiquitito. se agregaron componentes de login y de autenticación

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -3,10 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet], // ✅ IMPORTANTE
+  template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'web';
-}
+export class AppComponent {}
